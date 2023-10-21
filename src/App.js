60afa422007a610c3da52176c29c38ad5ff55511
reddivaraprasad <reddivaraprasad.786@gmail.com>
2023-10-21 16:34:08 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+
+const App = () => (
+  <>
+    <Header />
+    <Home />
+    <About />
+  </>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const Header = () => (
+  <nav>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+      />
+      <div>
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Header
